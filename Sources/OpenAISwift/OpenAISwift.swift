1288be081c729cdccb434fd541889d4ce20d2cd5
Nirjhar Biswas <75828293+Nirj2004@users.noreply.github.com>
2022-12-26 04:17:01 +0530

    New Files Added.

---
+++ Sources/OpenAISwift/OpenAISwift.swift
@@ -0,0 +1,114 @@
+import Foundation
+
+public enum OpenAIError: Error {
+    case genericError(error: Error)
+    case decodingError(error: Error)
+}
+
+public class OpenAISwift {
+    fileprivate(set) var token: String?
+    
+    public init(authToken: String) {
+        self.token = authToken
+    }
+}
+
+extension OpenAISwift {
+
+    public func sendCompletion(with prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 16, completionHandler: @escaping (Result<OpenAI, OpenAIError>) -> Void) {
+        let endpoint = Endpoint.completions
+        let body = Command(prompt: prompt, model: model.modelName, maxTokens: maxTokens)
+        let request = prepareRequest(endpoint, body: body)
+        
+        makeRequest(request: request) { result in
+            switch result {
+            case .success(let success):
+                do {
+                    let res = try JSONDecoder().decode(OpenAI.self, from: success)
+                    completionHandler(.success(res))
+                } catch {
+                    completionHandler(.failure(.decodingError(error: error)))
+                }
+            case .failure(let failure):
+                completionHandler(.failure(.genericError(error: failure)))
+            }
+        }
+    }
+    
+
+    public func sendEdits(with instruction: String, model: OpenAIModelType = .feature(.davinci), input: String = "", completionHandler: @escaping (Result<OpenAI, OpenAIError>) -> Void) {
+        let endpoint = Endpoint.edits
+        let body = Instruction(instruction: instruction, model: model.modelName, input: input)
+        let request = prepareRequest(endpoint, body: body)
+        
+        makeRequest(request: request) { result in
+            switch result {
+            case .success(let success):
+                do {
+                    let res = try JSONDecoder().decode(OpenAI.self, from: success)
+                    completionHandler(.success(res))
+                } catch {
+                    completionHandler(.failure(.decodingError(error: error)))
+                }
+            case .failure(let failure):
+                completionHandler(.failure(.genericError(error: failure)))
+            }
+        }
+    }
+    
+    private func makeRequest(request: URLRequest, completionHandler: @escaping (Result<Data, Error>) -> Void) {
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completionHandler(.failure(error))
+            } else if let data = data {
+                completionHandler(.success(data))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    private func prepareRequest<BodyType: Encodable>(_ endpoint: Endpoint, body: BodyType) -> URLRequest {
+        var urlComponents = URLComponents(url: URL(string: endpoint.baseURL())!, resolvingAgainstBaseURL: true)
+        urlComponents?.path = endpoint.path
+        var request = URLRequest(url: urlComponents!.url!)
+        request.httpMethod = endpoint.method
+        
+        if let token = self.token {
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+        }
+        
+        request.setValue("application/json", forHTTPHeaderField: "content-type")
+        
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(body) {
+            request.httpBody = encoded
+        }
+        
+        return request
+    }
+}
+
+extension OpenAISwift {
+
+    @available(swift 5.5)
+    @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+    public func sendCompletion(with prompt: String, model: OpenAIModelType = .gpt3(.davinci), maxTokens: Int = 16) async throws -> OpenAI {
+        return try await withCheckedThrowingContinuation { continuation in
+            sendCompletion(with: prompt, model: model, maxTokens: maxTokens) { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+
+    @available(swift 5.5)
+    @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
+    public func sendEdits(with instruction: String, model: OpenAIModelType = .feature(.davinci), input: String = "", completionHandler: @escaping (Result<OpenAI, OpenAIError>) -> Void) async throws -> OpenAI {
+        return try await withCheckedThrowingContinuation { continuation in
+            sendEdits(with: instruction, model: model, input: input) { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+}
